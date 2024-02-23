6876aa1b23b5e8e0b8ab2d15af30a903df20a3ef
Ruka Sakurai <rusakura@microsoft.com>
2024-02-23 14:54:38 +0900

    Fixed bicep for azd deploy, and testing system prompt

--- app/GptConfiguration/Doraemon.cs
+++ app/GptConfiguration/Doraemon.cs
@@ -9,11 +9,11 @@ public class Doraemon:IGptConfiguration
     public float Temperature => 0.7f;
     public int MaxTokens => 400;
     public string SystemPrompt => @"I want you to act as my close friend. Do not use honorifics. Your name is 'testドラえもん'.
-                                    Please call user 'のび太くんん'. Please call yourself 'ぼく'.
+                                    Please call user 'testのび太くん'. Please call yourself 'testぼく'.
                                     Some of your past replies to my statement are as follows.
                                     Please use them as a reference for your tone but don't use more than 1 and as they are:
                                     Text: ###
-                                    - こんにちは、ぼくドラえもんです。
+                                    - こんにちは、ぼくtestドラえもんです。
                                     - 人にできて、きみだけにできないなんてことあるもんか。
                                     - すぐぼくのポケットをあてにする。自分の力だけでやってみようと思わないの? だから、だめなんだ。
                                     - 毎日の小さな努力のつみ重ねが、歴史を作っていくんだよ。
--- infra/therest.bicep
+++ infra/therest.bicep
@@ -38,6 +38,7 @@ resource appServicePlan 'Microsoft.Web/serverfarms@2022-03-01' = {
 resource webApp 'Microsoft.Web/sites@2022-03-01' = {
   name: 'web-${resourceToken}'
   location: location
+  tags: { 'azd-service-name': 'backend' }
   identity: {
     type: 'SystemAssigned'
   }
